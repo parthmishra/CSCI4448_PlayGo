c872f1e3313c7a0c25e1edd1f36f9084dedf3a82
parthmishra <parthmishra@reshut1-32-222-dhcp.int.colorado.edu>
2016-04-09 18:26:08 -0600

    initial game commit

---
+++ playgo.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+class scoreBoard:
+
+	def getScore():
+		pass
+	
+	def getGameHistory():
+		pass
+
+	def endGame():
+		pass
+
+class Grid:
+
+	def getGrid():
+		pass
+
+	def calcValidity():
+		pass
+
+	def calcCapture():
+		pass
+
+class Player:
+	
+	def __init__(self, color, name):
+		self.color = color
+		self.name = name
+
+	def placePiece():
+		pass
+
+	def resign():
+		pass
+
+	def passMove(): # changed 'pass' to 'passMove' for clarity
+		pass
+
+# Test instantiation of Player 1
+"""
+name = "Test Player"
+color = "white"
+player1 = Player(color,name)
+print "Player name is: " + name + " and color is:", color
+"""
+
